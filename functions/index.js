5936aa5bdf0a2e9dc51f36250e46d3e4cea757f8
Dmitrijs Pavlovs <dmitrijs@hebsdigital.com>
2019-05-07 18:55:02 +0300

    Update

--- functions/index.js
+++ functions/index.js
@@ -10,30 +10,14 @@ api.use(express.static(__dirname + '/dist/'));
 
 const PORT = process.env.PORT || 9090;
   
-// api.route("/api/locations/*").get((req, res) => {
-
-// });
-api.get('/', (req, res) => {
-  res.send(process.env.NODE_ENV);
-  res.send("Referring to /");
-    request('http://andmebaas.stat.ee/sdmx-json/data/KK91', function (error, response, body) {
-        res.send(body);
-    });
-});
-
-api.get('/api', (req, res) => {
-    res.send(process.env.NODE_ENV);
-    res.send("Referring to /api");
-    request('http://andmebaas.stat.ee/sdmx-json/data/KK91', function (error, response, body) {
+api.get('/api/*', (req, res) => {
+    request('http://andmebaas.stat.ee/sdmx-json/data/'+req.params[0], function (error, response, body) {
         res.send(body);
     });
   });
   
-
-
-
 api.listen(PORT, () => {
-  console.log(`api listening on port ${PORT}`);
+  console.log(`NodeJS App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
 
