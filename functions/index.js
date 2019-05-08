5db1ebc4810c7e0016b3994aded8a1303fdfe911
Dmitrijs Pavlovs <dmitrijs@hebsdigital.com>
2019-05-08 11:37:17 +0300

    backend update

--- functions/index.js
+++ functions/index.js
@@ -10,11 +10,43 @@ api.use(express.static(__dirname + '/dist/'));
 
 const PORT = process.env.PORT || 9090;
   
-api.get('/api/*', (req, res) => {
-    request('http://andmebaas.stat.ee/sdmx-json/data/'+req.params[0], function (error, response, body) {
+
+//KK91
+
+api.get('/stat-ee/:tableName', (req, res) => {
+    console.log(req.params)
+    request('http://andmebaas.stat.ee/sdmx-json/data/'+req.params.tableName, function (error, response, body) {
         res.send(body);
     });
   });
+
+//   api.get('/stat-ee/:tableName/column-titles', (req, res) => {
+//     console.log(req.params)
+//     request('http://andmebaas.stat.ee/sdmx-json/data/'+req.params.tableName, function (error, response, body) {
+//         res.send(body);
+//     });
+//   });
+
+//   api.get('/stat-ee/:tableName/column-data', (req, res) => {
+//     console.log(req.params)
+//     request('http://andmebaas.stat.ee/sdmx-json/data/'+req.params.tableName, function (error, response, body) {
+//         res.send(body);
+//     });
+//   });
+
+//   api.get('/stat-ee/:tableName/row-data', (req, res) => {
+//     console.log(req.params)
+//     request('http://andmebaas.stat.ee/sdmx-json/data/'+req.params.tableName, function (error, response, body) {
+//         res.send(body);
+//     });
+//   });
+
+//   api.get('/stat-ee/:tableName/row-data', (req, res) => {
+//     console.log(req.params)
+//     request('http://andmebaas.stat.ee/sdmx-json/data/'+req.params.tableName, function (error, response, body) {
+//         res.send(body);
+//     });
+//   });
   
 api.listen(PORT, () => {
   console.log(`NodeJS App listening on port ${PORT}`);
